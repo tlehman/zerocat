bd9cb0fe3c95b30d3845865cc29533e1b77dc969
Tobi Lehman <tobi.lehman@gmail.com>
2017-01-28 14:51:15 -0800

    Copy Stdin to Stdout

--- main.go
+++ main.go
@@ -3,24 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/hashicorp/mdns"
-	"log"
+	"io"
 	"os"
 )
 
 func main() {
 	// turn off logging
-	devNull, _ := os.Open(os.DevNull)
-	log.SetOutput(devNull)
+	// devNull, _ := os.Open(os.DevNull)
+	// log.SetOutput(devNull)
 	server := createServer()
 	defer server.Shutdown()
-	performLookup()
+	//performLookup()
+	// send input to output
+
+	io.Copy(os.Stdout, os.Stdin)
 }
 
 func createServer() *mdns.Server {
 	host, _ := os.Hostname()
 	info := []string{"Zero configuration netcat"}
 	// TODO: Use https://github.com/phayes/freeport
-	service, err := mdns.NewMDNSService(host, "_zerocat._tcp", "", "", 8000, nil, info)
+	service, err := mdns.NewMDNSService(host,
+		"_zerocat._tcp", "", "",
+		8000, nil, info,
+	)
 	if err != nil {
 		fmt.Printf("error creating new mDNS service:  %v", err)
 	}
@@ -33,11 +39,14 @@ func performLookup() {
 	entriesCh := make(chan *mdns.ServiceEntry, 4)
 	go func() {
 		for entry := range entriesCh {
-			fmt.Printf("Got new entry: %v\n", entry)
+			fmt.Printf("Got new entry: %v\n", entry.Name)
 		}
 	}()
 
 	// Start the lookup
-	mdns.Lookup("_zerocat._tcp", entriesCh)
+	mdns.Query(&mdns.QueryParam{
+		Service: "_zerocat._tcp",
+		Entries: entriesCh,
+	})
 	close(entriesCh)
 }
